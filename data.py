83303b6aa40a0d28bf131fbc5aa133784b96dbef
Harsh bhakta <habhakta@ucsc.edu>
2020-02-28 15:45:20 -0800

    mhh for some reason this didn't push :(

--- data.py
+++ data.py
@@ -8,111 +8,147 @@ import numpy as np # Needed for histogram and other stuff
 import random # Needed to shuffle the data array
 import csv # Needed to write to a csv file
 
-data_dir = 'udacityData'
-# read the csv file
-fd = pd.read_csv(os.path.join(data_dir, 'driving_log.csv'))
+def splitPath(data):
+    front, end = ntpath.split(data)
+    # return the name of the image file
+    return "IMG/" + end
+
+def plotRawData(fd, fd_2):
+    # Plotting the steering angle data
+    i = np.linspace(0, 15539,15539)
+    #fd['steering'] = pd.to_numeric(fd['steering'])
+    #steering_list = np.asarray(fd['steering'])
+    steering_list = pd.concat([fd['steering'], fd_2['steering']])
+    
+    print(steering_list.shape)
+    
+    plt.title('Steering Angles vs. Data Points')
+    plt.xlabel('Steering Angles')
+    plt.ylabel('Data Points')
+
+    plt.plot(steering_list, i,'o')
+    plt.show()
+    '''Another way is to combine a set of steering angles in certain range
+        and group them together'''
+
+    num_of_buckets = 50       # Number of different groups(bucket) we want to create.
+    samples_per_bucket = 200    # Number of sample data per group(bucket).
 
 
-# Plotting the steering angle data
-i = np.linspace(0, 8035,8036)
-#fd['steering'] = pd.to_numeric(fd['steering'])
-
-plt.title('Steering Angles vs. Data Points')
-plt.xlabel('Steering Angles')
-plt.ylabel('Data Points')
-
-plt.plot(fd['steering'], i,'o')
-plt.show()
-'''Another way is to combine a set of steering angles in certain range
-   and group them together'''
-
-num_of_buckets = 50       # Number of different groups(bucket) we want to create.
-samples_per_bucket = 200    # Number of sample data per group(bucket).
+    # Generate a histogram and group the steering angles
+    histogram, buckets = np.histogram(steering_list, num_of_buckets)
+    # Print the amount of data per bucket
+    plt.title('Number of Steering Angle Data Points Specific to Each Bucket Range')
+    plt.xlabel('Bucketed Steering Angles')
+    plt.ylabel('Number of Steering Angle Data Points')
+    print(histogram)
+    # Find the center of the distribution.
+    center = (buckets[:-1] + buckets[1:])/2.0
+    # Plot the histogram as a bar graph
+    plt.bar(center,histogram, width=0.01) # Change the width to see better
+    plt.show()
 
 
-# Generate a histogram and group the steering angles
-histogram, buckets = np.histogram(fd['steering'], num_of_buckets)
-# Print the amount of data per bucket
-plt.title('Number of Steering Angle Data Points Specific to Each Bucket Range')
-plt.xlabel('Bucketed Steering Angles')
-plt.ylabel('Number of Steering Angle Data Points')
-print(histogram)
-# Find the center of the distribution.
-center = (buckets[:-1] + buckets[1:])/2.0
-# Plot the histogram as a bar graph
-plt.bar(center,histogram, width=0.01) # Change the width to see better
-plt.show()
+def balanceData(fd, fd_2, data_dir):
+    data_to_process = pd.concat([fd.head(fd.shape[0]), fd_2.head(fd_2.shape[0])], ignore_index=True)
+    #print(data_to_process.tail(35))
+    #print(data_to_process.shape)
+    
+    num_of_buckets = 50         # Number of different groups(bucket) we want to create.
+    samples_per_bucket = 300    # Number of sample data per group(bucket).
+    histogram, buckets = np.histogram(data_to_process['steering'], num_of_buckets)
+    
+    # list that will hold all the indices to be deleted from the original dataset
+    indices_to_delete = []
 
+    # Go through the entire data set and delete data for each bucket that has more than N samples
+    for b_i in range(len(buckets) - 1):
+        # list that will contain the index of sample data for each bucket.
+        bucket_list_indices = []
+        # There is probably a better way to do this on the entire list...
+        steering_val = 0.0
+        for i in range(len(data_to_process['steering'])):
+            steering_val = data_to_process['steering'][i]
+            # Check if the steering angle belongs to this bucket
+            if (steering_val >= buckets[b_i]) and (steering_val <= buckets[b_i + 1]):
+                bucket_list_indices.append(i)
 
-
-# We need to create an array/list that stores the randomly selected data
-# Copy the pointer of CSV file
-data_to_process = pd.read_csv(os.path.join(data_dir, 'driving_log.csv'))
-
-# list that will hold all the indices to be deleted from the original dataset
-indices_to_delete = []
-
-# Go through the entire data set and delete data for each bucket that has more than 200 samples
-for b_i in range(len(buckets) - 1):
-  # list that will contain the index of sample data for each bucket.
-  bucket_list_indices = []
-  # There is probably a better way to do this on the entire list...
-  steering_val = 0
-  for i in range(len(data_to_process['steering'])):
-    steering_val = data_to_process['steering'][i]
-    # Check if the steering angle belongs to this bucket
-    if (steering_val >= buckets[b_i]) and (steering_val <= buckets[b_i + 1]):
-      bucket_list_indices.append(i)
-
-  '''Now we have the list of samples for bucket b_i. Need to remove samples if
-  the len(bucket_list_indices) is greater than the number of samples per bucket
-  allowed. Before we add the indices to the delete list, need to make sure we
-  delete the datapoints randomly. The bucket_list_indices will contain indices
-  in increasing order therefore we need to shuffle the list so that we truly
-  delete data at random. '''
-  bucket_list_indices = random.sample(bucket_list_indices, len(bucket_list_indices))
-  # get all the indices after the bucket_list_indices[samples_per_bucket]
-  bucket_list_indices = bucket_list_indices[samples_per_bucket:]
-  # append the list for the bucket the overall data to be removed list
-  indices_to_delete.extend(bucket_list_indices)
+        '''Now we have the list of samples for bucket b_i. Need to remove samples if
+        the len(bucket_list_indices) is greater than the number of samples per bucket
+        allowed. Before we add the indices to the delete list, need to make sure we
+        delete the datapoints randomly. The bucket_list_indices will contain indices
+        in increasing order therefore we need to shuffle the list so that we truly
+        delete data at random. '''
+        print(len(bucket_list_indices))
+        bucket_list_indices = random.sample(bucket_list_indices, len(bucket_list_indices))
+        # get all the indices after the bucket_list_indices[samples_per_bucket]
+        bucket_list_indices = bucket_list_indices[samples_per_bucket:]
+        # append the list for the bucket the overall data to be removed list
+        indices_to_delete.extend(bucket_list_indices)
   
-''' https://thispointer.com/python-pandas-how-to-drop-rows-in-dataframe-by-index-labels/ '''
-# Finally drop the data we do not need....
-data_to_process.drop(data_to_process.index[indices_to_delete], inplace=True)
+    ''' https://thispointer.com/python-pandas-how-to-drop-rows-in-dataframe-by-index-labels/ '''
+    # Finally drop the data we do not need....
+    data_to_process.drop(data_to_process.index[indices_to_delete], inplace=True)
+    print(data_to_process.shape)
+    
+    # Need to reset the indexing otherwise we can not access the data properly:
+    # You could dump the file and reopen it to get the index, but idk how that will work
+    data_to_process.reset_index(inplace = True)
 
-#Need to reset the indexing otherwise we can not access the data properly:
-# You could dump the file and reopen it to get the index, but idk how that will work
-data_to_process.reset_index(inplace = True)
-
-
-
-
-with open(os.path.join(data_dir, 'balanced_data.csv'),'w') as output:
-    mywrite = csv.writer(output)
-    mywrite.writerow(['image', 'steering'])
-    for i in range(len(data_to_process['steering'])):
-        #print(data_to_process['center'][i], data_to_process['steering'][i])
-        mywrite.writerow([data_to_process['center'][i].strip(), data_to_process['steering'][i]])
-        mywrite.writerow([data_to_process['left'][i].strip(), data_to_process['steering'][i]])
-        mywrite.writerow([data_to_process['right'][i].strip(), data_to_process['steering'][i]])
-
-##END ADDED
+    print(data_to_process.shape)
 
 
-#data_to_process.to_csv(os.path.join(data_dir, 'balanced_data.csv'))
+    with open(os.path.join(data_dir, 'balanced_data.csv'),'w') as output:
+        mywrite = csv.writer(output)
+        mywrite.writerow(['image', 'steering'])
+        for i in range(len(data_to_process['steering'])):
+            #print(data_to_process['center'][i], data_to_process['steering'][i])
+            mywrite.writerow([data_to_process['center'][i].strip(), data_to_process['steering'][i]])
+            mywrite.writerow([data_to_process['left'][i].strip(), data_to_process['steering'][i]])
+            mywrite.writerow([data_to_process['right'][i].strip(), data_to_process['steering'][i]])
 
-# histogram and group the steering angles
-histogram, buckets = np.histogram(data_to_process['steering'], num_of_buckets)
-# Print the amount of data per bucket
-plt.title('Balanced Data: Number of Steering Angle Data Points Specific to Each Bucket Range')
-plt.xlabel('Bucketed Steering Angles')
-plt.ylabel('Number of Steering Angle Data Points')
-print(buckets)
-# Find the center of the distribution.
-center = (buckets[:-1] + buckets[1:])/2.0
-print(center)
-# Plot the histogram as a bar graph
-plt.bar(center,histogram, width=0.01) # Change the width to see better
-plt.show()
+    ##END ADDED
 
+
+    #data_to_process.to_csv(os.path.join(data_dir, 'balanced_data.csv'))
+
+    # histogram and group the steering angles
+    histogram, buckets = np.histogram(data_to_process['steering'], num_of_buckets)
+    # Print the amount of data per bucket
+    plt.title('Balanced Data: Number of Steering Angle Data Points Specific to Each Bucket Range')
+    plt.xlabel('Bucketed Steering Angles')
+    plt.ylabel('Number of Steering Angle Data Points')
+    print(buckets)
+    # Find the center of the distribution.
+    center = (buckets[:-1] + buckets[1:])/2.0
+    print(center)
+    # Plot the histogram as a bar graph
+    plt.bar(center,histogram, width=0.01) # Change the width to see better
+    plt.show()
+
+
+#Main function of the file.
+def main():
+    udacity_data_dir = 'udacityData'
+    our_data_dir = 'finalData'
+    data_dir = 'newSpreadsheet'
+    # read the csv file
+    fd = pd.read_csv(os.path.join(udacity_data_dir, 'driving_log.csv'))
+    
+    col_names = ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
+    fd_2 = pd.read_csv(os.path.join(our_data_dir, 'driving_log_new.csv'), names = col_names)
+    
+    # plot the raw data from both directories.
+    #plotRawData(fd, fd_2)
+    
+    #Do Some Stripping
+    fd_2['center'] = fd_2['center'].apply(splitPath)
+    fd_2['left'] = fd_2['left'].apply(splitPath)
+    fd_2['right'] = fd_2['right'].apply(splitPath)
+    
+    #print(fd_2.head(30))
+    balanceData(fd, fd_2, data_dir)
+
+if __name__ == "__main__":
+    main()
 
